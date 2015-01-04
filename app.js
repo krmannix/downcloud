21eb203293376763cf9ec3f30c993df776d55613
Kevin Mannix <kmannix@bu.edu>
2015-01-03 19:38:04 -0500

    Can search users, and select a user

--- app.js
+++ app.js
@@ -1,2 +1,69 @@
-var http = require('http');
+/* MODULE IMPORTS */
+var https = require('https');
+
+/* SET UP USER INPUT */
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+
+/* GLOBAL VARIABLES */
+var host = "https://api-v2.soundcloud.com";
+var limit = 10;
+var user;
+var stdout = process.stdout;
+var stdin = process.stdin;
+
+/* START OF PROCESS */
+/* Check for user input */
+// Do this later
+// var args = process.argv.slice(2);
+// if (arg_user.length > 0) {
+// 	user = args[0];
+// } else {
+
+// }
+
+stdout.write("Search for a SoundCloud user: ");
+stdin.once('data', function(data) {
+	searchRequest(data);
+});
+
+/* REQUESTS */
+var searchRequest = function(user) {
+	https.get(host + "/search/users?q=" + user + "&limit=" + limit, function(response) {
+		var body = "";
+		response.on("data", function(chunk) {
+			body += chunk;
+		});
+		response.on("end", function() {
+			if (response.statusCode == 200) {
+				chooseSearchResultRequest(body);
+			} else {
+				console.log("HTTP Error: " + response.statusCode);
+			}
+		});
+		response.on("error", function(e) {
+			console.log("HTTP Error: " + e.message);
+		});
+	});
+}
+
+var chooseSearchResultRequest = function(body) {
+	var json = JSON.parse(body);
+	var nextURL = json.next_href;
+	var collection = json.collection;
+	var users = [];
+	// process.exit();
+	for (var i = 0; i < collection.length; i++) {
+		users.push({username: collection[i].username, id: collection[i].id});
+		console.log(i + ": " + collection[i].username);
+	}
+	stdout.write("Enter [0-" + collection.length + "] to select a user or n for next 10 users: ");
+}
+
+
+
+
+
+
+
 
